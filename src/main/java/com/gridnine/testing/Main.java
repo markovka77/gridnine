db2dea4dcc66c0f0bd0200f71840a132a9c726d3
Mark <voronkov1mark@gmail.com>
2024-01-15 01:32:51 +0300

    выводит cегменты с датой прилёта раньше даты вылета.

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -3,8 +3,12 @@ package com.gridnine.testing;
 public class Main {
 
     public static void main(String[] args) {
-       //Service.outputTestSet();
+       Service.outputTestSet();
+        System.out.println();
         System.out.println(Service.outputFlightWhereDepartureToTheCurrentPointInTime());
+        System.out.println();
+        System.out.println(Service.outputFlightWhereArrivalDateBeforeDepartureDate());
+//        System.out.println(Service.outputFlightWhereTotalTimeSpentOnEarthExceedsTwoHours());
 
     }
 
--- src/main/java/com/gridnine/testing/Service.java
+++ src/main/java/com/gridnine/testing/Service.java
@@ -1,25 +1,51 @@
 package com.gridnine.testing;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Service {
 
-    public static LocalDateTime dateTimeNow = LocalDateTime.now();
-    static void outputTestSet(){
+    static LocalDateTime dateTimeNow = LocalDateTime.now();
+
+    static void outputTestSet() {
         //вывод в консоль результатов обработки тестового набора перелётов
         FlightBuilder.createFlights()
                 .forEach(System.out::println);
     }
-    static List<Flight> outputFlightWhereDepartureToTheCurrentPointInTime(){
-       return FlightBuilder.createFlights().stream()
+
+    static List<Flight> outputFlightWhereDepartureToTheCurrentPointInTime() {
+        return FlightBuilder.createFlights().stream()
                 .filter(flight -> flight.getSegments().stream()
                         .allMatch(segment -> segment.getDepartureDate()
                                 .isBefore(dateTimeNow)))
-               .collect(Collectors.toList());
-
-
+                .collect(Collectors.toList());
     }
+
+    static List<Flight> outputFlightWhereArrivalDateBeforeDepartureDate() {
+        return FlightBuilder.createFlights().stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .allMatch(segment -> segment.getArrivalDate()
+                                .isBefore(segment.getDepartureDate())))
+                .collect(Collectors.toList());
+    }
+
+//    static List<Flight> outputFlightWhereTotalTimeSpentOnEarthExceedsTwoHours() {
+//      List<Flight> twoHoursAtEarth = FlightBuilder.createFlights().stream()
+//              .filter(flight -> flight.getSegments().size()>=2).toList();
+//
+//       List<Segment>segments = twoHoursAtEarth.stream().filter(flight -> flight.getSegments().stream()
+//                       .allMatch(segment -> segment.getArrivalDate().plusHours(2).isAfter(segment.getDepartureDate())))
+//               .
+//
+//
+//       return FlightBuilder.createFlights().stream()
+//                .filter(flight -> flight.getSegments().stream()
+//                        .anyMatch(segment -> segment.getArrivalDate().plusHours(2)
+//                                .isAfter(segment.getDepartureDate())))
+//               .collect(Collectors.toList());
+//    }
+
 }
