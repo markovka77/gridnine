f5e38f98ecdcedb55390c86edb41a2f8fec5c019
Mark <voronkov1mark@gmail.com>
2024-01-21 00:03:41 +0300

    поправить метод с двумя часами

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
        List<Flight>allFls = FlightBuilder.createFlights();
-//       Service.outputFlightWhereDepartureToTheCurrentPointInTime(allFls);
-       Service.outputFlightWhereArrivalDateBeforeDepartureDate(allFls);
-      // Service.outputFlightWhereTotalTimeSpentOnEarthExceedsTwoHours(allFls);
+      // Service.outputFlightWhereDepartureToTheCurrentPointInTime(allFls);
+       //Service.outputFlightWhereArrivalDateBeforeDepartureDate(allFls);
+       Service.outputFlightWhereTotalTimeSpentOnEarthExceedsTwoHours(allFls);
 
 
     }
--- src/main/java/com/gridnine/testing/Service.java
+++ src/main/java/com/gridnine/testing/Service.java
@@ -2,6 +2,7 @@ package com.gridnine.testing;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,35 +25,36 @@ public class Service {
     //выводим  список, в котором вылет до текущего момента времени
 
     public static void outputFlightWhereDepartureToTheCurrentPointInTime(List<Flight>allFls) {
-        List<Flight> list1 = allFls.stream()
+        List<Flight> test = new ArrayList<>(allFls);
+        List<Flight> list1 = new ArrayList<>(allFls.stream()
                 .filter(flight -> flight.getSegments().stream()
                         .allMatch(segment -> segment.getDepartureDate()
-                                .isBefore(dateTimeNow)))
-                .toList();
+                                .isBefore(dateTimeNow))).toList());
         System.out.println("Все полёты, кроме тех, в которых отправление раньше текущего времени:");
-                allFls.stream()
-                .filter(flight -> !flight.equals(list1.get(list1.size() - 1)))
-                .toList().forEach(System.out::println);
+        test.removeAll(list1);
+        test.forEach(System.out::println);
     }
 
     //выводим список, в котором сегменты с датой прилёта раньше даты вылета
 
     public static void outputFlightWhereArrivalDateBeforeDepartureDate(List<Flight>allFls) {
-        List<Flight> list2 = allFls.stream()
+        List<Flight> test = new ArrayList<>(allFls);
+        List<Flight> list2 =new ArrayList<>( allFls.stream()
                 .filter(flight -> flight.getSegments().stream()
                         .allMatch(segment -> segment.getArrivalDate()
                                 .isBefore(segment.getDepartureDate())))
-                .toList();
+                .toList());
         System.out.println("Все полёты, кроме тех, в которых отправление раньше даты прибытия: ");
-        allFls.stream()
-                .filter(flight -> !flight.equals(list2.get(list2.size()-1)))
-                .toList().forEach(System.out::println);
+//
+        test.removeAll(list2);
+        test.forEach(System.out::println);
     }
 
 
     //выводим список, перелетов, где общее время, проведённое на земле, превышает два часа
 
     public static void outputFlightWhereTotalTimeSpentOnEarthExceedsTwoHours(List<Flight>allFls) {
+        List<Flight> test = new ArrayList<>(allFls);
         List<Flight> list3 = new ArrayList<>();
         for(Flight flight:allFls) {
             List<Segment> seg = flight.getSegments();
@@ -65,9 +67,11 @@ public class Service {
         }
         System.out.println("Все полёты,кроме тех, в которых общее время на земле 2 или более часов:");
 
-        allFls.stream()
-                .filter(flight -> !flight.equals(list3.get(list3.size()-1)))
-                .toList().forEach(System.out::println);
+//        allFls.stream()
+//                .filter(flight -> !flight.equals(list3.get(list3.size()-1)))
+//                .toList().forEach(System.out::println);
+        test.removeAll(list3);
+        test.forEach(System.out::println);
     }
 
 
